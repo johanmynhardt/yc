4fb3117fa14146df915c847a739654aa98244ca3
Johan Mynhardt <johanmynhardt@gmail.com>
2017-10-21 15:28:33 +0200

    Link up routing and pages in core (WIP).

--- src/yc_app/core.cljs
+++ src/yc_app/core.cljs
@@ -1,11 +1,23 @@
 (ns yc-app.core
-    (:require [rum.core :as rum]))
+    (:require [rum.core :as rum]
+              [accountant.core :as accountant]
+              [bidi.bidi :as bidi]
+              [secretary.core :as secretary :refer-macros [defroute]]
+              [yc-app.navigation :as navigation]
+              [yc-app.pages :as pages]))
 
 (enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+
+;---- link up page and route
+(defmulti page-contents identity)
+
+(defmethod page-contents :home [] (pages/home))
+(defmethod page-contents :default [] (pages/not-found))
+
 
 (rum/defc menu-button []
   [:button.header-button {:onClick #(js/console.log "echo")}])
@@ -37,11 +49,27 @@
     (for [x (range 0 15)]
       [:p "a generated parapgraph " x])]])
 
-(rum/mount (yc)
-           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (rum/mount (yc)
+             (. js/document (getElementById "app")))
 )
+
+(defn ^:export init! []
+  (accountant/configure-navigation!
+   {:nav-handler (fn [path]
+                   (let [match (bidi/match-route navigation/app-routes path)
+                         current-page (:handler match)
+                         route-params (:route-params match)]
+                     (println "current-page: " current-page)
+                     (comment  (session/put! :route {:current-page current-page
+                                                     :route-params route-params}))))
+    :path-exists? (fn [path]
+                    (boolean (bidi/match-route navigation/app-routes path)))})
+  (accountant/dispatch-current!)
+  (on-js-reload))
+
+(init!)
